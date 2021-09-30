fb57a6c271342a453918393ec2f4f7f0c7d62341
luis rodriguez <kike.lerj2710@gmail.com>
2021-09-30 08:48:07 -0500

    listar los autos en el HTML

--- js/app.js
+++ js/app.js
@@ -1,11 +1,29 @@
 //variables
-const 
+const resultado = document.querySelector('#resultado');
 
 
 //eventos
-
+document.addEventListener('DOMContentLoaded',()=>{
+ mostarAutos();
+});
 
 
 
 
 //funciones
+
+function mostarAutos() {
+    autos.forEach( auto =>{
+const {marca, modelo, year, precio, color, transmision, puertas }= auto
+       const autoHtml = document.createElement('p');
+       autoHtml.innerHTML=`
+       ${marca} ${modelo}  Año:${year} De:${puertas} Precio: ${precio} Color:${color} transmision: ${transmision}
+       
+       `;
+
+       //mostar en el HTML
+       resultado.appendChild(autoHtml);
+    });
+
+   
+}
